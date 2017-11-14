2bb11c0cca76903078738d6a9f3e6073cda389a3
chenhaotian <chenhaotian.jtt@gmail.com>
2017-11-14 17:17:23 +0100

    truth 2

--- R/TFR.r
+++ R/TFR.r
@@ -1,26 +1,3 @@
-## data
-rawdb <- unique(data.frame(e=sample(letters[1:4],30,replace = TRUE),a=sample(c("spa","breakfast","iron"),size = 30,replace = TRUE),s=sample(toupper(letters[1:3]),30,replace = TRUE),stringsAsFactors = FALSE))
-
-if(any(duplicated(rawdb))) stop("duplicated entries in input data")
-
-## facts
-facts <- unique(rawdb[,c("e","a")])
-facts <- facts[order(facts$e,facts$a),]
-
-## claims
-
-factsource <- unique(rawdb[,c("e","s")])
-
-lapply(split(facts,1:nrow(facts)),function(d){
-    
-})
-
-lapply(split(rawdb,rawdb$e),function(d){
-    claims <- expand.grid(d$a,d$s,stringsAsFactors = FALSE)
-    claims$o <- claims %IN% d[,c("a","s")]
-    claims
-})
-
 ## data.fram version of %in%
 ## returns a logical vector indicating if there is a match or not for EACH ROW of its left operand.
 `%IN%` <- function(df1,df2){
@@ -46,10 +23,91 @@ lapply(split(rawdb,rawdb$e),function(d){
     }))
 }
 
-a br
-b sp
+## label:
+## e: entity
+## a: attribute
+## s: source
+## o: claim
+## t: truth
 
-a br
-a sp
-b br
-b sp
+## raw data
+rawdb <- unique(data.frame(e=sample(letters[1:4],30,replace = TRUE),a=sample(c("spa","breakfast","iron"),size = 30,replace = TRUE),s=sample(toupper(letters[1:3]),30,replace = TRUE),stringsAsFactors = FALSE))
+
+if(any(duplicated(rawdb))) stop("duplicated entries in input data")
+
+## mappers
+factsmapper <- unique(rawdb[,c("e","a")])
+factsmapper$fid <- 1:nrow(factsmapper)
+
+sourcesmapper <- unique(rawdb[,c("s")])
+sourcesmapper <- data.frame(s=sourcesmapper,sid=1:length(sourcesmapper),stringsAsFactors = FALSE)
+
+## claims
+claims <- do.call(rbind.data.frame,lapply(split(rawdb,rawdb$e),function(d){
+    tmp <- expand.grid(unique(d$a),unique(d$s),stringsAsFactors = FALSE)
+    names(tmp) <- c("a","s")
+    tmp$o <- as.integer(tmp %IN% d[,c("a","s")])
+    tmp$e <- d$e[1]
+    tmp
+}))
+row.names(claims) <- NULL
+claims <- merge(merge(claims,factsmapper,all.x = TRUE,sort = FALSE)[c("fid","s","o")],sourcesmapper,all.x = TRUE,sort = FALSE)[c("fid","sid","o")]
+claims <- claims[order(claims$fid,claims$sid,claims$o),]
+
+## initialize truth
+facts <- data.frame(fid=factsmapper$fid,t=sample(c(0,1),nrow(factsmapper),replace = TRUE),stringsAsFactors = FALSE)
+facts <- facts[order(facts$fid),]
+
+## count, truth-source-claim
+## raw sensitivity and FPR data
+ctsc <- expand.grid(c(0,1),sourcesmapper$sid,c(0,1),stringsAsFactors = FALSE)
+names(ctsc) <- c("t","sid","o")
+cwit <- merge(claims,facts,all.x = TRUE,sort = FALSE) # claim with initial truth, a tmp variable in calculating ctsc
+ctsc <- merge(ctsc,aggregate(fid~sid+t+o,data = cwit,FUN = length),all.x = TRUE,sort = FALSE)
+rm(cwit)
+names(ctsc)[4] <- "count"
+ctsc$count[is.na(ctsc$count)] <- 0L
+ctsc <- ctsc[order(ctsc$t,ctsc$sid,ctsc$o),]
+
+## fact-claim index, claim start posiitons of each fact
+## to simplify C++ code, here the index starts with zero
+fcidx <- match(facts$fid,claims$fid)-1             #facts and claims must be sorted beforehand
+fcidx <- c(fcidx,nrow(claims))                     #append end position for easier representation
+
+
+input: facts claims fcidx ctsc
+
+for( f in fid){
+    
+    cf <- claims[claims$fid==f,]
+    ## get startidx and endidx of f in claim
+    
+    for(c in cf){
+        calculate p with ctsc
+    }
+
+    update facts
+    for(c in cf){
+        update ctsc
+    }
+}
+
+
+
+## claims <- merge(claims,factsmapper,by = c("e","a"),all.x = TRUE)
+
+## claims$t <- facts$t[claims$fid]
+
+## s <- unique(rawdb$s)
+## t <- c(TRUE,FALSE)
+## o <- c(TRUE,FALSE)
+
+
+
+
+## (fid)-(source,claim) mapper
+fsc <- subset(claims,select=c(fid,s,o))
+
+
+
+## count of source-attribute-claim
---
+++ src/TFR.cpp
@@ -0,0 +1,134 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+#define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+#define PBWIDTH 60
+
+// [[Rcpp::export]]
+void printProgress (double percentage)
+{
+    int val = (int) (percentage * 100);
+    int lpad = (int) (percentage * PBWIDTH);
+    int rpad = PBWIDTH - lpad;
+    printf ("\r%3d%% [%.*s%*s]", val, lpad, PBSTR, rpad, "");
+    fflush (stdout);
+}
+
+// [[Rcpp::export]]
+int one_cat(NumericVector probs){
+  int k = probs.size();
+  int res = 0;
+  double z=sum(probs);
+  if(z!=1){
+    for(int i=0;i<k;++i){
+      probs[i]=probs[i]/z;
+    }
+  }
+  IntegerVector ans(k);
+  rmultinom(1, probs.begin(), k, ans.begin());
+  for(int j=0;j<k;++j){
+    if(ans[j]==1){
+      res=j+1;
+      break;
+    }
+  }
+  return res;
+}
+
+
+input: facts claims ctsc
+
+for( f in fid){
+    
+    cf <- claims[claims$fid==f,]
+    ## get startidx and endidx of f in claim
+    
+    for(c in cf){
+        calculate p with ctsc
+    }
+
+    update facts
+    for(c in cf){
+        update ctsc
+    }
+}
+
+// [[Rcpp::export]]
+List getcvk(IntegerVector facts, IntegerMatrix claims, IntegerMatrix ctsc, int maxit, int sumL, int K,int V, IntegerVector L,double a, double g){
+  int doc=0,word=0,topic=0,idx=0;
+  NumericVector probs(K);
+  int pwt_idx=0;		     // indicating P(w|t) storage index
+  int pwt_rows=(int)maxit/100;
+  int pwt_columns=(int)(K+K*V);
+  int VK=(int)V*K;
+  IntegerMatrix pwt_raw(pwt_rows,pwt_columns);
+  
+  // main gibbs loop
+  for(int j = 0; j < maxit; ++j){
+    // sub-steps
+    for(int i = 0; i < sumL; ++i){
+      doc=dwt(i,0);
+      word=dwt(i,1);
+      topic=dwt(i,2);
+
+      // std::cout << doc << " " << word << " " << topic << std::endl;
+      // std::cout << i << std::endl;
+      
+      // cvk[topic,word] <- cvk[topic,word]-1
+      idx=(topic-1)*V+word-1;
+      cvk(idx,2)=cvk(idx,2)-1;
+      // cik[doc,topic] <- cik[doc,topic]-1
+      idx=(doc-1)*K+topic-1;
+      cik(idx,2)=cik(idx,2)-1;
+      // std::cout <<  cik(idx,2) << std::endl;
+      // ck[topic] <- ck[topic]-1 #ck must be ordered!
+      ck[topic-1]=ck[topic-1]-1;
+      // probs <- (cvk[,word]+g)/(ck+V*g) * (cik[doc,]+a)/(L[doc]+K*a)
+      for(int k = 0; k < K; ++k){
+	probs[k]= ((double)cvk(k*V+word-1,2)+g)/
+	  ((double)ck[k]+(double)V*g)*
+	  ((double)cik((doc-1)*K+k,2)+a)/
+	  ((double)L[doc-1]+(double)K*a);
+      }
+      // topic <- Cat(probs)
+      topic=one_cat(probs);
+      // std::cout << probs[0] << std::endl;
+      // dwt$topic[i] <- topic               #replace the old topic
+      dwt(i,2)=topic;
+      // cvk[topic,word] <- cvk[topic,word]+1
+      idx=(topic-1)*V+word-1;
+      cvk(idx,2)=cvk(idx,2)+1;
+      // cik[doc,topic] <- cik[doc,topic]+1
+      idx=(doc-1)*K+topic-1;
+      cik(idx,2)=cik(idx,2)+1;
+      // ck[topic] <- ck[topic]+1
+      ck[topic-1]=ck[topic-1]+1;
+      // if(i>=4665500){
+      // 	std::cout << i << std::endl;
+      // }
+    }
+
+    // store raw data in pwt_raw to calculate log(P(w|t))
+    if(j%(int)100 == 99){
+      pwt_idx=(int)j/(int)100;
+      for(int l = 0; l < VK; ++l){
+    	pwt_raw(pwt_idx,l)=cvk(l,2);
+      }
+      for(int l = VK; l < pwt_columns; ++l){
+    	pwt_raw(pwt_idx,l)=ck[l-VK];
+      }
+    }
+    // std::cout << j << "th iteration done" << std::endl;
+    printProgress((double)j/maxit);
+  }
+
+  // return Rcpp::List::create(Rcpp::Named("cvk") = cvk,
+  // 			    Rcpp::Named("dwt") = dwt,
+  // 			    Rcpp::Named("cik") = cik,
+  // 			    Rcpp::Named("ck") = ck);
+    return Rcpp::List::create(Rcpp::Named("cvk") = cvk,
+  			    Rcpp::Named("dwt") = dwt,
+  			    Rcpp::Named("cik") = cik,
+  			    Rcpp::Named("ck") = ck,
+  			    Rcpp::Named("pwt_raw") = pwt_raw);
+}
+
