1617ac450d86ed050099e80d1b0bea223c572a7a
chenhaotian <chen-haotian@qq.com>
2017-11-16 00:04:09 +0100

    modify trutch finding wrapper

--- R/TFR.r
+++ R/TFR.r
@@ -33,91 +33,61 @@
 ## raw data
 rawdb <- unique(data.frame(e=sample(letters[1:4],30,replace = TRUE),a=sample(c("spa","breakfast","iron"),size = 30,replace = TRUE),s=sample(toupper(letters[1:3]),30,replace = TRUE),stringsAsFactors = FALSE))
 
-if(any(duplicated(rawdb))) stop("duplicated entries in input data")
+    function input:
+    beta: Fx2 numeric matrix, each row is the beta prior the corresponding fact
+    alpha0: Sx2 numeric matrix, each row is the beta prior for the corresponding source FPR, each element is at least 1
+    alpha1: Sx2 numeric matrix, each row is the beta prior for the corresponding source sensitivity, each element is at least 1
 
-## mappers
-factsmapper <- unique(rawdb[,c("e","a")])
-factsmapper$fid <- 1:nrow(factsmapper)
 
-sourcesmapper <- unique(rawdb[,c("s")])
-sourcesmapper <- data.frame(s=sourcesmapper,sid=1:length(sourcesmapper),stringsAsFactors = FALSE)
-
-## claims
-claims <- do.call(rbind.data.frame,lapply(split(rawdb,rawdb$e),function(d){
+TF_binary <- function(rawdb,beta,alpha0,alpha1){
+    if(is.matrix(rawdb)) rawdb <- as.data.frame(rawdb)
+    if(!is.data.frame(rawdb)) stop("rawdb must be a data.frame or matrix with exactly 3 columns")
+    if(ncol(rawdb)!=3) stop("rawdb doesn't have 3 columns")
+    if(any(duplicated(rawdb))) stop("duplicated entries in rawdb")
+    
+    
+    ## mappers
+    factsmapper <- unique(rawdb[,c("e","a")])
+    factsmapper$fid <- 0:(nrow(factsmapper)-1)
+    
+    sourcesmapper <- unique(rawdb[,c("s")])
+    sourcesmapper <- data.frame(s=sourcesmapper,sid=0:(length(sourcesmapper)-1),stringsAsFactors = FALSE)
+    
+    ## claims
+    claims <- do.call(rbind.data.frame,lapply(split(rawdb,rawdb$e),function(d){
     tmp <- expand.grid(unique(d$a),unique(d$s),stringsAsFactors = FALSE)
     names(tmp) <- c("a","s")
     tmp$o <- as.integer(tmp %IN% d[,c("a","s")])
     tmp$e <- d$e[1]
     tmp
 }))
-row.names(claims) <- NULL
-claims <- merge(merge(claims,factsmapper,all.x = TRUE,sort = FALSE)[c("fid","s","o")],sourcesmapper,all.x = TRUE,sort = FALSE)[c("fid","sid","o")]
-claims <- claims[order(claims$fid,claims$sid,claims$o),]
-
-## initialize truth
-facts <- data.frame(fid=factsmapper$fid,t=sample(c(0,1),nrow(factsmapper),replace = TRUE),stringsAsFactors = FALSE)
-facts <- facts[order(facts$fid),]
-
-## count, truth-source-claim
+    row.names(claims) <- NULL
+    claims <- merge(merge(claims,factsmapper,all.x = TRUE,sort = FALSE)[c("fid","s","o")],sourcesmapper,all.x = TRUE,sort = FALSE)[c("fid","sid","o")]
+    claims <- claims[order(claims$fid,claims$sid,claims$o),]
+    
+    ## initialize truth
+    facts <- data.frame(fid=factsmapper$fid,t=sample(c(0,1),nrow(factsmapper),replace = TRUE),stringsAsFactors = FALSE)
+    facts <- facts[order(facts$fid),]
+    
+    ## count, truth-source-claim
 ## raw sensitivity and FPR data
-ctsc <- expand.grid(c(0,1),sourcesmapper$sid,c(0,1),stringsAsFactors = FALSE)
-names(ctsc) <- c("t","sid","o")
-cwit <- merge(claims,facts,all.x = TRUE,sort = FALSE) # claim with initial truth, a tmp variable in calculating ctsc
-ctsc <- merge(ctsc,aggregate(fid~sid+t+o,data = cwit,FUN = length),all.x = TRUE,sort = FALSE)
-rm(cwit)
-names(ctsc)[4] <- "count"
-ctsc$count[is.na(ctsc$count)] <- 0L
-ctsc <- ctsc[order(ctsc$t,ctsc$sid,ctsc$o),]
-
-## fact-claim index, claim start posiitons of each fact
+    ctsc <- expand.grid(c(0,1),sourcesmapper$sid,c(0,1),stringsAsFactors = FALSE)
+    names(ctsc) <- c("t","sid","o")
+    cwit <- merge(claims,facts,all.x = TRUE,sort = FALSE) # claim with initial truth, a tmp variable in calculating ctsc
+    ctsc <- merge(ctsc,aggregate(fid~sid+t+o,data = cwit,FUN = length),all.x = TRUE,sort = FALSE)
+    rm(cwit)
+    names(ctsc)[4] <- "count"
+    ctsc$count[is.na(ctsc$count)] <- 0L
+    ctsc <- ctsc[order(ctsc$t,ctsc$sid,ctsc$o),]
+    
+    ## fact-claim index, claim start posiitons of each fact
 ## to simplify C++ code, here the index starts with zero
-fcidx <- match(facts$fid,claims$fid)-1L             #facts and claims must be sorted beforehand
-fcidx <- c(fcidx,nrow(claims))                      #append end position for easier representation
+    fcidx <- match(facts$fid,claims$fid)-1L             #facts and claims must be sorted beforehand
+    fcidx <- c(fcidx,nrow(claims))                      #append end position for easier representation
 
-function input:
-beta: Fx2 numeric matrix, each row is the beta prior the corresponding fact
-alpha0: Sx2 numeric matrix, each row is the beta prior for the corresponding source FPR
-alpha1: Sx2 numeric matrix, each row is the beta prior for the corresponding source sensitivity
+##     input: facts(integervector) claims(integermatrix) fcidx(integervector) ctsc(integermatrix) beta(numericmatrix) alpha0(numericmatrix) alpha1(numericmatrix)
+## claims: fid sid o
+## ctsc:   t   sid o count
 
-
-
-input: facts(integervector) claims(integermatrix) fcidx(integervector) ctsc(integermatrix) beta(numericmatrix) alpha0(numericmatrix) alpha1(numericmatrix)
-claims: fid sid o
-ctsc:   t   sid o count
-
-
-
-for( f in fid){
     
-    cf <- claims[claims$fid==f,]
-    ## get startidx and endidx of f in claim
-    
-    for(c in cf){
-        calculate p with ctsc
-    }
-
-    update facts
-    for(c in cf){
-        update ctsc
-    }
 }
-
-
-
-## claims <- merge(claims,factsmapper,by = c("e","a"),all.x = TRUE)
-
-## claims$t <- facts$t[claims$fid]
-
-## s <- unique(rawdb$s)
-## t <- c(TRUE,FALSE)
-## o <- c(TRUE,FALSE)
-
-
-
-
-## (fid)-(source,claim) mapper
-fsc <- subset(claims,select=c(fid,s,o))
-
-
-
-## count of source-attribute-claim
--- src/TFR.cpp
+++ src/TFR.cpp
@@ -34,137 +34,68 @@ int one_cat_zero_begin(NumericVector probs){
   return res;
 }
 
-input: facts(vector) claims fcidx(vector) ctsc beta alpha0 alpha1
-
-for( f in fid){
-    
-    cf <- claims[claims$fid==f,]
-    ## get startidx and endidx of f in claim
-    
-    for(c in cf){
-        calculate p with ctsc
-    }
-
-    update facts
-    for(c in cf){
-        update ctsc
-    }
-}
-
 // [[Rcpp::export]]
-List truthfinding(IntegerVector facts,IntegerVector fcidx, IntegerMatrix claims, IntegerMatrix ctsc, NumericMatrix beta, NumericMatrix alpha0, NumericMatrix alpha1){
+List truthfinding_binary(IntegerVector facts,IntegerVector fcidx, IntegerMatrix claims, IntegerMatrix ctsc, NumericMatrix beta, NumericMatrix alpha0, NumericMatrix alpha1){
   int nfacts=facts.size();
+  int nsources=alpha0.row();
+  int expand_source_claim=nsources*2;
+  int idx=0;
   int startidx=0,endidx=0;	// claim start and end index for each fact
+  int sid=0,o=0,t=0;		// tmp variable in locating claims in ctsc
+  int fact_pre=0,fact_aft=0;
   NumericVector probs(2);
-  double conditional_claim=1;
+  double conditional_claim0=1, conditional_claim1=1;
   
   // main gibbs loop
-  // for each fact f...
+  // 1. for each fact f...
   for(int f = 0; f < nfacts; ++f){
     startidx=fcidx[f];
     endidx=fcidx[f+1];
-    // generate sample probabilities
-    for(int t = 0; t < 2; ++t){
-      // reset conditional claim probability
-      conditional_claim=1;
-      // for each claim c in fact f...
-      for(int c = startidx; c < endidx; ++c){
-	conditional_claim = conditional_claim*
-      }
-      probs[t] = beta(f,t)*conditional_claim
-    }
-    // sample and update facts
-    one_cat_zero_begin(probs);
+    fact_pre=facts[f];
+    
+    // 2. for each claim c in fact f...
+    for(int c = startidx; c < endidx; ++c){
+      sid=claims(c,1);
+      o=claims(c,2);
 
+      // 2.0 generate sample probability for t=0
+      t=0;			// when f is true
+      conditional_claim0=1;	//reset conditional claim probability
+      idx=sid*2;
+      conditional_claim0 = conditional_claim0*
+	((double)ctsc(idx+o,3)+alpha0(sid,o))/ //equation (2)
+	((double)ctsc(idx,3)+(double)ctsc(idx+1,3)+alpha0(sid,0)+alpha0(sid,1));
+
+      // 2.1 generate sample probability for t=1
+      t=1;			// when f is false
+      conditional_claim1=1;	// reset conditional claim probability
+      idx= expand_source_claim + idx
+      conditional_claim1=conditional_claim1*
+	((double)ctsc(idx+o,3)+alpha1(sid,o))/ //equation (2)
+	((double)ctsc(idx,3)+(double)ctsc(idx+1,3)+alpha1(sid,0)+alpha1(sid,1));
+    }
+
+    probs[0] = beta(f,0)*conditional_claim0;
+    probs[1] = beta(f,1)*conditional_claim1;
+    
+    // sample and update facts
+    fact_aft=one_cat_zero_begin(probs);
     
     // update ctsc
     for(int c = startidx; c < endidx; ++c){
-      
+      sid=claims(c,1);
+      o=claims(c,2);
+
+      // pre - 1
+      idx=fact_pre*expand_source_claim+sid*2+o;
+      ctsc(idx,3)=ctsc(idx,3)-1;
+
+      // aft + 1
+      idx=fact_aft*expand_source_claim+sid*2+o;
+      ctsc(idx,3)=ctsc(idx,3)+1;
     }
     
   }
-  
-  
 }
 
-
-// [[Rcpp::export]]
-List truthfinding(IntegerVector facts, IntegerMatrix claims, IntegerMatrix ctsc, int maxit, int sumL, int K,int V, IntegerVector L,double a, double g){
-  int doc=0,word=0,topic=0,idx=0;
-  NumericVector probs(K);
-  int pwt_idx=0;		     // indicating P(w|t) storage index
-  int pwt_rows=(int)maxit/100;
-  int pwt_columns=(int)(K+K*V);
-  int VK=(int)V*K;
-  IntegerMatrix pwt_raw(pwt_rows,pwt_columns);
-  
-  // main gibbs loop
-  for(int j = 0; j < maxit; ++j){
-    // sub-steps
-    for(int i = 0; i < sumL; ++i){
-      doc=dwt(i,0);
-      word=dwt(i,1);
-      topic=dwt(i,2);
-
-      // std::cout << doc << " " << word << " " << topic << std::endl;
-      // std::cout << i << std::endl;
-      
-      // cvk[topic,word] <- cvk[topic,word]-1
-      idx=(topic-1)*V+word-1;
-      cvk(idx,2)=cvk(idx,2)-1;
-      // cik[doc,topic] <- cik[doc,topic]-1
-      idx=(doc-1)*K+topic-1;
-      cik(idx,2)=cik(idx,2)-1;
-      // std::cout <<  cik(idx,2) << std::endl;
-      // ck[topic] <- ck[topic]-1 #ck must be ordered!
-      ck[topic-1]=ck[topic-1]-1;
-      // probs <- (cvk[,word]+g)/(ck+V*g) * (cik[doc,]+a)/(L[doc]+K*a)
-      for(int k = 0; k < K; ++k){
-	probs[k]= ((double)cvk(k*V+word-1,2)+g)/
-	  ((double)ck[k]+(double)V*g)*
-	  ((double)cik((doc-1)*K+k,2)+a)/
-	  ((double)L[doc-1]+(double)K*a);
-      }
-      // topic <- Cat(probs)
-      topic=one_cat(probs);
-      // std::cout << probs[0] << std::endl;
-      // dwt$topic[i] <- topic               #replace the old topic
-      dwt(i,2)=topic;
-      // cvk[topic,word] <- cvk[topic,word]+1
-      idx=(topic-1)*V+word-1;
-      cvk(idx,2)=cvk(idx,2)+1;
-      // cik[doc,topic] <- cik[doc,topic]+1
-      idx=(doc-1)*K+topic-1;
-      cik(idx,2)=cik(idx,2)+1;
-      // ck[topic] <- ck[topic]+1
-      ck[topic-1]=ck[topic-1]+1;
-      // if(i>=4665500){
-      // 	std::cout << i << std::endl;
-      // }
-    }
-
-    // store raw data in pwt_raw to calculate log(P(w|t))
-    if(j%(int)100 == 99){
-      pwt_idx=(int)j/(int)100;
-      for(int l = 0; l < VK; ++l){
-    	pwt_raw(pwt_idx,l)=cvk(l,2);
-      }
-      for(int l = VK; l < pwt_columns; ++l){
-    	pwt_raw(pwt_idx,l)=ck[l-VK];
-      }
-    }
-    // std::cout << j << "th iteration done" << std::endl;
-    printProgress((double)j/maxit);
-  }
-
-  // return Rcpp::List::create(Rcpp::Named("cvk") = cvk,
-  // 			    Rcpp::Named("dwt") = dwt,
-  // 			    Rcpp::Named("cik") = cik,
-  // 			    Rcpp::Named("ck") = ck);
-    return Rcpp::List::create(Rcpp::Named("cvk") = cvk,
-  			    Rcpp::Named("dwt") = dwt,
-  			    Rcpp::Named("cik") = cik,
-  			    Rcpp::Named("ck") = ck,
-  			    Rcpp::Named("pwt_raw") = pwt_raw);
-}
 
