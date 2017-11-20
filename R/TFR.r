3b82f6f71cf6235e2f4c3ee2af70519594de5465
chenhaotian <chenhaotian.jtt@gmail.com>
2017-11-20 15:54:07 +0100

    truth finding debug

--- R/TFR.r
+++ R/TFR.r
@@ -30,7 +30,6 @@ checkprior <- function(prior,CLASS=character(),NROW=NULL,NCOL=NULL,LENGTH=NULL,E
 library(Rcpp)
 sourceCpp("src/TFR.cpp")
 
-
 ## note that in LTM, "recall" and "specificity" of an source are calculated with regard to all the entities that contain claims from this source, this is only meaning full when there're multiple right attributes associated to each entitiy. When there's only one right attribute in each entity, there's no FN and TN in the confusion matrix, only "precision" can be calculated.
 ## for example:
 ## Entity e has right attributes (a1,a2,a3), source s1 has one claim a1, source s2 has three claims (a2,a3,a4), then the recall for s1 and s2 are both 1/3, while the specificity for s1 and s2 are 1 and 0, one must use both recall and specificity to measure s1 and s2's qualities. What if e has only one right attribute, which is a1, s1 has claim a1 and s2 has claim a2, then the precision for s1 and s2 are 1 and 0, and only precision alone is able to measure qualities of this two sources.
@@ -46,7 +45,7 @@ sourceCpp("src/TFR.cpp")
 
 ## alpha0: , hyper parameter for specificity when model="mn". NULL for model="sn" and "ss"
 ## alpha1: hyper parameter for recall when model="mn", hyper parameter for precision when model="sn" and "ss"
-TF <- function(rawdb,model=c("mn","sn","ss"),beta,alpha0,alpha1,burnin,maxit,sample_step){
+TF <- function(rawdb,model=c("mn","sn","ss"),beta,alpha0=NULL,alpha1,burnin,maxit,sample_step){
 
     cat("Checking integrity...")
     burnin <- as.integer(burnin)
@@ -54,7 +53,9 @@ TF <- function(rawdb,model=c("mn","sn","ss"),beta,alpha0,alpha1,burnin,maxit,sam
     sample_step<- as.integer(sample_step)
     ## integrity check part1
     if(is.matrix(rawdb)) rawdb <- as.data.frame(rawdb)
-    if(!checkprior(rawdb,CLASS = "data.frame",NCOL = 3) | any(duplicated(rawdb))) stop("rawdb must be a data.frame or matrix with 3 columns and no duplicate entries"){
+    if(!checkprior(rawdb,CLASS = "data.frame",NCOL = 3) | any(duplicated(rawdb))){
+        stop("rawdb must be a data.frame or matrix with 3 columns representing entity, attribute and source respectively")
+    }else{
         names(rawdb) <- c("e","a","s")
     }
     model <- match.arg(model)
@@ -70,118 +71,121 @@ TF <- function(rawdb,model=c("mn","sn","ss"),beta,alpha0,alpha1,burnin,maxit,sam
         stop("beta should be a length 2 numberic when model = mn, a length 1 numeric when model = sn, a lengh number_of_attributes vector when model = ss")
     cat("done.\n")
 
-    cat("Preparing mappers...")
-    ## 1. mappers
+
+
+    sourcesmapper <- unique(rawdb[,"s",drop=FALSE])
+    sourcesmapper$sid <- 0L:(nsourcecs-1L)
     if(model=="mn"){
+        cat("Preparing mappers...")
         factsmapper <- unique(rawdb[,c("e","a")])
         factsmapper$fid <- 0L:(nrow(factsmapper)-1L)
-    }else if(model=="sn"){
+        cat("done.\n")
+
+        cat("Preparing claims...")
+        claims <- do.call(rbind.data.frame,lapply(split(rawdb,rawdb$e),function(d){
+            tmp <- expand.grid(unique(d$a),unique(d$s),stringsAsFactors = FALSE)
+            names(tmp) <- c("a","s")
+            tmp$o <- as.integer(tmp %IN% d[,c("a","s")])
+            tmp$e <- d$e[1]
+            tmp
+        }))
+        row.names(claims) <- NULL
+        claims <- merge(merge(claims,factsmapper,all.x = TRUE,sort = FALSE)[c("fid","s","o")],sourcesmapper,all.x = TRUE,sort = FALSE)[c("fid","sid","o")]
+        claims <- claims[order(claims$fid,claims$sid,claims$o),]
+        cat("done.\n")
+
+        cat("Initializing truth & truth-source-claim count...")
+        facts <- data.frame(fid=factsmapper$fid,t=sample(c(0L,1L),nrow(factsmapper),replace = TRUE),stringsAsFactors = FALSE)
+        facts <- facts[order(facts$fid),]
+        ## count, truth-source-claim
+        ## raw sensitivity and FPR data
+        ctsc <- expand.grid(c(0L,1L),sourcesmapper$sid,c(0L,1L),stringsAsFactors = FALSE)
+        names(ctsc) <- c("t","sid","o")
+        cwit <- merge(claims,facts,all.x = TRUE,sort = FALSE) # claim with initial truth, a tmp variable in calculating ctsc
+        ctsc <- merge(ctsc,aggregate(fid~sid+t+o,data = cwit,FUN = length),all.x = TRUE,sort = FALSE)
+        rm(cwit)
+        names(ctsc)[4] <- "count"
+        ctsc$count[is.na(ctsc$count)] <- 0L
+        ctsc <- ctsc[order(ctsc$t,ctsc$sid,ctsc$o),]
+        ## fact-claim index, claim start posiitons of each fact
+        ## to simplify C++ code, here the index starts with zero
+        fcidx <- match(facts$fid,claims$fid)-1L             #facts and claims must be sorted beforehand
+        fcidx <- c(fcidx,nrow(claims))                      #append end position for easier representation
+        cat("done.\n")
+
+        cat("Sampling...\n")
+        res <- truthfinding_mn(facts=facts$t,fcidx=fcidx, claims=as.matrix(claims), ctsc=as.matrix(ctsc), beta=beta, alpha0=alpha0, alpha1=alpha1,burnin = burnin, maxit = maxit,sample_step = sample_step)
+        cat("\n all done \n")
+        
+    }
+    else if(model=="sn"){
+        cat("Preparing mappers...")
         entities <- unique(rawdb[,"e",drop=FALSE])
         entities$eid <- 0L:(nentities-1L)
+        rawdb <- rawdb[order(rawdb$e),] #group by e, in order to split
+        attributesmapper <- rawdb[,c("e","a")]
+        attributesmapper$aid <- unlist(sapply(split(rawdb$a,rawdb$e),function(a){
+            match(a,unique(a))-1L
+        },simplify = FALSE,USE.NAMES = FALSE),use.names = FALSE)
+        ## tokenize rawdb
+        rawdb_original <- rawdb         #save a backup
+        rawdb$a <- attributesmapper$aid
+        rawdb$e <- entities$eid[match(rawdb$e,entities$e)]
+        rawdb$s <- sourcesmapper$sid[match(rawdb$s,sourcesmapper$s)]
+        rawdb <- rawdb[order(rawdb$e,rawdb$a,rawdb$s),]  # !!! important!!!
+        attributesmapper <- unique(attributesmapper)
+        cat("done.\n")
+        
+        cat("Initializing counts & turths ...")
+        e_n_attr <- aggregate(a~e,data = rawdb,function(l){length(unique(l))})
+        e_n_attr <- e_n_attr$a[order(e_n_attr$e)] #claimed attributes count for each entity
+        s_n_claims <- aggregate(e~s,data = rawdb,length)
+        s_n_claims <- s_n_claims$e[order(s_n_claims$s)] #claims of each source(some claims may be too few)
+        max_nattributes <- max(e_n_attr)
+        e_truths <- sapply(e_n_attr,function(l){ #initialize truth for each entity
+            one_cat_zero_begin(rep(1,l+1)) #?????? l+1 or l ???????
+        },simplify = TRUE,USE.NAMES = FALSE)
+        s_n_right <- sapply(split(rawdb$a==e_truths[match(rawdb$e,entities$eid)],rawdb$s),sum,simplify = TRUE,USE.NAMES = TRUE)
+        s_n_right <- s_n_right[order(as.integer(names(s_n_right)))] #number of right cliams for each source
+        ecidx <- c(match(entities$eid,rawdb$e)-1L,nrow(rawdb))
+        cat("done.\n")
+
+        cat("Sampling...\n")
+        res <- truthfinding_sn(ecidx = ecidx,e_truths = e_truths,e_n_attr = e_n_attr,s_n_right = s_n_right, s_n_claims = s_n_claims,rawdb = as.matrix(rawdb),beta = beta, alpha1 = alpha1, max_nattributes = max_nattributes, burnin = burnin, maxit = maxit,sample_step = sample_step)
+        cat("\n all done \n")
     }
-    sourcesmapper <- unique(rawdb[,c("s")])
-    sourcesmapper <- data.frame(s=sourcesmapper,sid=0L:(length(sourcesmapper)-1L),stringsAsFactors = FALSE)
 
 
-    cat("Preparing claims...")
-    ## 2. claims
-    claims <- do.call(rbind.data.frame,lapply(split(rawdb,rawdb$e),function(d){
-        tmp <- expand.grid(unique(d$a),unique(d$s),stringsAsFactors = FALSE)
-        names(tmp) <- c("a","s")
-        tmp$o <- as.integer(tmp %IN% d[,c("a","s")])
-        tmp$e <- d$e[1]
-        tmp
-    }))
-    row.names(claims) <- NULL
-    claims <- merge(merge(claims,factsmapper,all.x = TRUE,sort = FALSE)[c("fid","s","o")],sourcesmapper,all.x = TRUE,sort = FALSE)[c("fid","sid","o")]
-    claims <- claims[order(claims$fid,claims$sid,claims$o),]
-    cat("done.\n")
-
-    cat("Initializing truth & truth-source-claim count...")
-    ## 3. initialize truth
-    facts <- data.frame(fid=factsmapper$fid,t=sample(c(0L,1L),nrow(factsmapper),replace = TRUE),stringsAsFactors = FALSE)
-    facts <- facts[order(facts$fid),]
-    
-    ## count, truth-source-claim
-    ## raw sensitivity and FPR data
-    ctsc <- expand.grid(c(0L,1L),sourcesmapper$sid,c(0L,1L),stringsAsFactors = FALSE)
-    names(ctsc) <- c("t","sid","o")
-    cwit <- merge(claims,facts,all.x = TRUE,sort = FALSE) # claim with initial truth, a tmp variable in calculating ctsc
-    ctsc <- merge(ctsc,aggregate(fid~sid+t+o,data = cwit,FUN = length),all.x = TRUE,sort = FALSE)
-    rm(cwit)
-    names(ctsc)[4] <- "count"
-    ctsc$count[is.na(ctsc$count)] <- 0L
-    ctsc <- ctsc[order(ctsc$t,ctsc$sid,ctsc$o),]
-    
-    ## fact-claim index, claim start posiitons of each fact
-    ## to simplify C++ code, here the index starts with zero
-    fcidx <- match(facts$fid,claims$fid)-1L             #facts and claims must be sorted beforehand
-    fcidx <- c(fcidx,nrow(claims))                      #append end position for easier representation
-    cat("done.\n")
-
-    
-    cat("Sampling...\n")
-    res <- truthfinding_mn(facts=facts$t,fcidx=fcidx, claims=as.matrix(claims), ctsc=as.matrix(ctsc), beta=beta, alpha0=alpha0, alpha1=alpha1,burnin = burnin, maxit = maxit,sample_step = sample_step)
-    cat("\n all done \n")
-
-    res$ctsc <- ctsc
-    res$claims <- claims
-    res$factsmapper <- factsmapper
-    res$sourcesmapper <- sourcesmapper
+    ## res$ctsc <- ctsc
+    ## res$claims <- claims
+    ## res$factsmapper <- factsmapper
+    ## res$sourcesmapper <- sourcesmapper
     return(res)
 }
 
-
-load("~/Downloads/crowd_task_am_history")
-
-ones <- crowd[,c("tracking_id","decision","user_id","decision")]; names(ones) <- c("e","a","s","o")
-ones$o <- 1L
-zeros <- crowd[,c("tracking_id","decision","user_id","qa_decision")]; names(zeros) <- c("e","a","s","o")
-
-rawdb <- rbind(ones,zeros)
-rawdb <- unique(rawdb)
 
 beta=1;alpha0=1;alpha1=1;burnin=100;maxit=1000;sample_step=30
 
 testdata <- rawdb[rawdb$e %in% sample(rawdb$e,10000),]
 
-res <- TF_binary(rawdb = testdata,binary = TRUE,beta = 1,alpha0 = matrix(c(8,2),nrow = length(unique(testdata$s)),ncol = 2,byrow = TRUE),alpha1 = 0.1,burnin = 500,maxit = 2000,sample_step = 10)
+## res <- TF_binary(rawdb = testdata,binary = TRUE,beta = 1,alpha0 = matrix(c(8,2),nrow = length(unique(testdata$s)),ncol = 2,byrow = TRUE),alpha1 = 0.1,burnin = 500,maxit = 2000,sample_step = 10)
 
-
+load("~/Downloads/crowd_task_am_history")
 rawdb <- crowd[,c("tracking_id","decision","user_id")]
 names(rawdb) <- c("e","a","s")
-testdata <- rawdb[rawdb$e %in% sample(rawdb$e,10000),]
+rawdb <- rawdb[rawdb$e %in% sample(rawdb$e,20000),]
 
-res <- TF_binary(rawdb = testdata,beta = 1,alpha0 = matrix(c(8,2),nrow = length(unique(testdata$s)),ncol = 2,byrow = TRUE),alpha1 = 0.1,burnin = 500,maxit = 2000,sample_step = 10)
-
-res <- TF_binary(rawdb = testdata,beta = matrix(c(8,2),nrow = nrow(unique(testdata[,c("e","a")])),ncol = 2,byrow = TRUE) ,alpha0 = 0.1,alpha1 = 0.1,burnin = 500,maxit = 2000,sample_step = 10)
-
-res <- TF_binary(rawdb = testdata,beta = 1,alpha0 = 0.1,alpha1 = 0.1,burnin = 500,maxit = 2000,sample_step = 10)
-
-## crowd <- merge(crowd,x,by.x = "external_task_id",by.y = "task_id",all.x = FALSE, all.y = FALSE)
+xxx <- aggregate(e~s,data = rawdb,length)
+rawdb <- rawdb[rawdb$s%in%xxx$s[xxx$e > quantile(xxx$e,0.2)],]
 
 
 
-## unique(data.frame(e=sample(letters,3000,replace = TRUE),a=sample(c("spa","breakfast","iron"),size = 3000,replace = TRUE),t=sample(c(1,1,1,1,0),3000,replace = TRUE),stringsAsFactors = FALSE))
+res <- TF(rawdb = rawdb,model = "sn",beta = 1,alpha1 = matrix(c(8,2),nrow = length(unique(rawdb$s)),ncol = 2,byrow = TRUE),burnin = 1000,maxit = 3000,sample_step = 100)
 
+res <- TF(rawdb = rawdb,model = "sn",beta = 1,alpha1 = matrix(1,nrow = length(unique(rawdb$s)),ncol = 2),burnin = 1000,maxit = 10000,sample_step = 100)
 
-## e <- unique(replicate(1000,paste(sample(letters,5),collapse = "")))
-## o <- unique(replicate(1000,paste(sample(letters,2),collapse = "")))
+library(reshape2)
+spa <- read.table("~/Downloads/consolidation_spa.csv",header = TRUE,sep = ",",quote = "\"",na.strings = "NA",stringsAsFactors = FALSE)
+spa <- spa[,-1]
+spa <- na.omit(melt(spa,id.vars="item_id"))
 
-## tmp <- replicate(length(e),sample(o,sample(1:5)))
-## truth <- data.frame(e=rep(e,times=sapply(tmp,length)),a=unlist(tmp),stringsAsFactors = FALSE)
-
-## s <- unique(replicate(1000,paste(sample(letters[1:3],2),collapse = "")))
-## specificities <- runif(length(s),min = 0.5,max = 0.9)
-## recalls <- runif(length(s),min = 0.5,max = 0.9)
-
-
-## label:
-## e: entity
-## a: attribute
-## s: source
-## o: claim
-## t: truth
-
-## raw data
-rawdb <- unique(data.frame(e=sample(letters,3000,replace = TRUE),a=sample(c("spa","breakfast","iron"),size = 3000,replace = TRUE),s=sample(toupper(letters[1:3]),300,replace = TRUE),stringsAsFactors = FALSE))
--- src/TFR.cpp
+++ src/TFR.cpp
@@ -34,7 +34,7 @@ int one_cat_zero_begin(NumericVector probs){
   return res;
 }
 
-// mn: each entity has MULTIPLE true attributes, attributes NOT sharing among entities.
+// mn: each entity has MULTIPLE true attributes, attributes are NOT sharing among entities.
 // [[Rcpp::export]]
 List truthfinding_mn(IntegerVector facts,IntegerVector fcidx, IntegerMatrix claims, IntegerMatrix ctsc, NumericVector beta, NumericMatrix alpha0, NumericMatrix alpha1,int burnin, int maxit, int sample_step){
 
@@ -159,9 +159,9 @@ List truthfinding_mn(IntegerVector facts,IntegerVector fcidx, IntegerMatrix clai
 			    Rcpp::Named("specificity") = specificity);
 }
 
-// sn: each entity has SINGLE true attribute, attributes NOT sharing among entities.
+// sn: each entity has SINGLE true attribute, attributes are NOT sharing among entities.
 // [[Rcpp::export]]
-List truthfinding_sn(IntegerVector ecidx, IntegerVector e_truths, IntegerVector e_n_attr, IntegerVector s_n_right, IntegerVector s_n_claims, IntegerMatrix rawdb, Numeric beta, NumericMatrix alpha1,int max_nattributes, int burnin, int maxit, int sample_step){
+List truthfinding_sn(IntegerVector ecidx, IntegerVector e_truths, IntegerVector e_n_attr, IntegerVector s_n_right, IntegerVector s_n_claims, IntegerMatrix rawdb, double beta, NumericMatrix alpha1,int max_nattributes, int burnin, int maxit, int sample_step){
 
   // ctsc:   t   sid o count
   int nentities=e_truths.size();
@@ -197,33 +197,47 @@ List truthfinding_sn(IntegerVector ecidx, IntegerVector e_truths, IntegerVector 
 
       // 2. for each attribute, calculate it's probability of being right
       // note that a<=nattributes, the nattributes-th iteration is to calculate the probability that none of the claims are right
-      for(int a = 0; a <= nattributes; ++a){
-	// initialize 
-	probs[a]=1;
+      for(int a = 0; a < nattributes; ++a){
+      // for(int a = 0; a <= nattributes; ++a){
+	// initialize & prior
+	probs[a]=beta;
 	for(int i = startidx; i < endidx; ++i){
 	  sid=rawdb(i,2);
-	  if(rawdb(i,1)==truth_pre){ // current claim is right
-	    probs[a] = probs[a]*
-	      beta*		// prior, p(a|t)
-	      ((double)s_n_right[sid]- (double)1 + alpha1(sid,0))/
-	      ((double)s_n_claims[sid]- (double)1 + alpha1(sid,0) + alpha1(sid,0));
-	  }else{		// current claim is wrong
-	    probs[a] = probs[a]*
-	      beta*		// prior, p(a|t)
-	      ((double)s_n_right[sid] + alpha1(sid,0))/
-	      ((double)s_n_claims[sid]- (double)1 + alpha1(sid,0) + alpha1(sid,0));
+	  if(rawdb(i,1)==a){	       // p(a|claim)
+	    if(rawdb(i,1)==truth_pre){
+	      probs[a] = probs[a]*
+		((double)s_n_right[sid]- (double)1 + alpha1(sid,0))/
+		((double)s_n_claims[sid]- (double)1 + alpha1(sid,0) + alpha1(sid,0));
+	    }else{
+	      probs[a] = probs[a]*
+		((double)s_n_right[sid] + alpha1(sid,0))/
+		((double)s_n_claims[sid]- (double)1 + alpha1(sid,0) + alpha1(sid,0));
+	    }
+	  }else{		       // p(hat(a)|claim)
+	    if(rawdb(i,1)==truth_pre){
+	      probs[a] = probs[a]*
+		((double)1 - 
+		 ((double)s_n_right[sid]- (double)1 + alpha1(sid,0))/
+		 ((double)s_n_claims[sid]- (double)1 + alpha1(sid,0) + alpha1(sid,0)));
+	    }else{
+	      probs[a] = probs[a]*
+		((double)1 - 
+		 ((double)s_n_right[sid] + alpha1(sid,0))/
+		 ((double)s_n_claims[sid]- (double)1 + alpha1(sid,0) + alpha1(sid,0)));
+	    }
 	  }
 	}
       }
-      
-      e_truths[eid]=one_cat_zero_begin(probs[seq(0,nattributes)]);
+
+      e_truths[eid]=one_cat_zero_begin(probs[seq(0,nattributes-1)]);
+      // e_truths[eid]=one_cat_zero_begin(probs[seq(0,nattributes)]);
       
       // update
       if(truth_pre != e_truths[eid]){
 	for(int i = startidx; i < endidx; ++i){
 	  sid=rawdb(i,2);
 	  if(rawdb(i,1)==truth_pre){ // minus
-	    s_n_right[sid] = s_n_right[sid] + 1;
+	    s_n_right[sid] = s_n_right[sid] - 1;
 	  }else if(rawdb(i,1)==e_truths[eid]){ // plus
 	    s_n_right[sid] = s_n_right[sid] + 1;
 	  }
@@ -261,128 +275,143 @@ List truthfinding_sn(IntegerVector ecidx, IntegerVector e_truths, IntegerVector 
 			    Rcpp::Named("precision") = precision);
 }
 
-
-// ss: each entity has SINGLE true attribute, attributes SHARING among entities.
+// ss: each entity has SINGLE true attribute, attributes are SHARING among entities.
 // [[Rcpp::export]]
-List truthfinding_ss(IntegerVector facts,IntegerVector fcidx, IntegerMatrix claims, IntegerMatrix ctsc, NumericMatrix beta, NumericMatrix alpha0, NumericMatrix alpha1,int burnin, int maxit, int sample_step){
-  
-  // claims: fid sid o
-  // ctsc:   t   sid o count
-  
-  int nfacts=facts.size();
-  int nsources=alpha0.nrow();
-  int expand_source_claim=nsources*2;
-  int idx=0;
+List truthfinding_ss(IntegerVector ecidx, IntegerVector e_truths, IntegerVector s_a_n_right, IntegerVector s_a_n_claims, IntegerMatrix rawdb, NumericVector beta, NumericVector pi, NumericMatrix alpha1,int nattributes, int nsources, int nentities, int burnin, int maxit, int sample_step){
+
+  // // remove:
+  // e_n_attr;
+  // max_nattributes;
+
+  // // add:
+  // nattributes;
+  // nsources;
+  // pi;
+
+  // // change:
+  // s_n_right = s_a_n_right;
+  // s_n_claims = s_a_n_claims;
+  // length(beta) = nattributes;
+  // dim(alpha1) = (nsources*nattributes) x 2;
+  // sid = idx;
+  int nsourceattributes = nsources*nattributes;
   int startidx=0,endidx=0;	// claim start and end index for each fact
-  int sid=0,o=0,t=0;		// tmp variable in locating claims in ctsc
-  int fact_pre=0;
-  NumericVector probs(2);
-  double conditional_claim0=1, conditional_claim1=1;
+  int idx=0;   		// tmp variable in locating claims
+  int truth_pre=0;
+  NumericVector probs(nattributes); // initialize probability vector
   
+  if(beta.size()!=nattributes){
+    std::cout << "length(beta) not equal to number of unique attributes!" << std::endl;
+    std::exit(-1);
+  }
+  if(alpha1.nrow()!=nsourceattributes){
+    std::cout << "nrow(alpha1) not equal to (number_of_unique_sources) x (number_of_unique_attributes)!" << std::endl;
+    std::exit(-1);
+  }
+
   // outputs
   int sample_size = maxit/sample_step - burnin/sample_step;
   if(sample_size <=0){
     std::cout << "sample_size = maxit/sample_step - burnin/sample_step <=0!" << std::endl;
     std::exit(-1);
   }
-  NumericVector facts_out(nfacts,(double)0);
-  NumericVector ctsc_out(ctsc.nrow(),(double)0);
-  NumericVector recall(nsources,(double)0);
-  NumericVector specificity(nsources,(double)0);
-  NumericVector precision(nsources,(double)0);
+  NumericVector s_a_n_right_out(nsourceattributes,(double)0);
+  NumericVector precision(nsourceattributes,(double)0);
+
+  // rawdb:
+  // entity attribute source
   
   // main gibbs loop
   int it = 0;
   while(it < maxit){
-    // 1. for each fact f...
-    for(int f = 0; f < nfacts; ++f){
-      startidx=fcidx[f];		// fact->claim index
-      endidx=fcidx[f+1];
-      fact_pre=facts[f];
-      
-      // 2. for each claim c in fact f...
-      for(int c = startidx; c < endidx; ++c){
-	sid=claims(c,1);
-	o=claims(c,2);
-	
-	// 2.0 generate sample probability for t=0
-	// alpha0: prior for specificity
-	t=0;			// when f is false
-	conditional_claim0=1;	//reset conditional claim probability
-	idx=sid*2;
-	conditional_claim0 = conditional_claim0*
-	  ((double)ctsc(idx+o,3)-(double)1+alpha0(sid,o))/ //equation (2)
-	  ((double)ctsc(idx,3)+(double)ctsc(idx+1,3)-(double)1+alpha0(sid,0)+alpha0(sid,1));
-	
-	// 2.1 generate sample probability for t=1
-	// alpha1: prior for recall
-	t=1;			// when f is true
-	conditional_claim1=1;	// reset conditional claim probability
-	idx= expand_source_claim + idx;
-	conditional_claim1=conditional_claim1*
-	  ((double)ctsc(idx+o,3)-(double)1+alpha1(sid,1-o))/ //equation (2)
-	  ((double)ctsc(idx,3)+(double)ctsc(idx+1,3)-(double)1+alpha1(sid,0)+alpha1(sid,1));
-      }
-      
-      probs[0] = beta(f,1)*conditional_claim0;
-      probs[1] = beta(f,0)*conditional_claim1;
-      
-      // 3. sample and update facts
-      facts[f]=one_cat_zero_begin(probs);
-      
-      // 4. update ctsc
-      if(facts[f] != fact_pre){
-	for(int c = startidx; c < endidx; ++c){
-	  sid=claims(c,1);
-	  o=claims(c,2);
-	  
-	  // pre - 1
-	  idx=fact_pre*expand_source_claim+sid*2+o;
-	  ctsc(idx,3)=ctsc(idx,3)-1;
-	  
-	  // aft + 1
-	  idx=facts[f]*expand_source_claim+sid*2+o;
-	  ctsc(idx,3)=ctsc(idx,3)+1;
+    // 1. for each entity ...
+    for(int eid = 0; eid < nentities; ++eid){
+      // initialize indexes
+      startidx=ecidx[eid];
+      endidx=ecidx[eid+1];
+      truth_pre=e_truths[eid];
+
+      // 2. for each attribute, calculate it's probability of being right
+      for(int a = 0; a < nattributes; ++a){
+	// initialize & prior
+	if(truth_pre==a){
+	  probs[a] = (double)(pi[a]-1);
+	}else{
+	  probs[a] = (double)pi[a];
+	}
+	for(int i = startidx; i < endidx; ++i){
+	  idx=rawdb(i,2)*nattributes+rawdb(i,1);
+	  if(rawdb(i,1)==a){	       // p(a|claim)
+	    if(rawdb(i,1)==truth_pre){
+	      probs[a] = probs[a]*
+		((double)s_a_n_right[idx]- (double)1 + alpha1(idx,0))/
+		((double)s_a_n_claims[idx]- (double)1 + alpha1(idx,0) + alpha1(idx,0));
+	    }else{
+	      probs[a] = probs[a]*
+		((double)s_a_n_right[idx] + alpha1(idx,0))/
+		((double)s_a_n_claims[idx]- (double)1 + alpha1(idx,0) + alpha1(idx,0));
+	    }
+	  }else{		       // p(hat(a)|claim)
+	    if(rawdb(i,1)==truth_pre){
+	      probs[a] = probs[a]*
+		((double)1 -
+		 ((double)s_a_n_right[idx]- (double)1 + alpha1(idx,0))/
+		 ((double)s_a_n_claims[idx]- (double)1 + alpha1(idx,0) + alpha1(idx,0)));
+	    }else{
+	      probs[a] = probs[a]*
+		((double)1 -
+		 ((double)s_a_n_right[idx] + alpha1(idx,0))/
+		 ((double)s_a_n_claims[idx]- (double)1 + alpha1(idx,0) + alpha1(idx,0)));
+	    }
+	  }
 	}
       }
+
+      e_truths[eid]=one_cat_zero_begin(probs);
+      
+      // update
+      if(truth_pre != e_truths[eid]){
+	// update pi
+	pi[truth_pre] = pi[truth_pre]-1;
+	pi[e_truths[eid]] = pi[e_truths[eid]]+1;
+	// update s_a_n_right
+	for(int i = startidx; i < endidx; ++i){
+	  idx=rawdb(i,2)*nattributes+rawdb(i,1);
+	  if(rawdb(i,1)==truth_pre){ // minus
+	    s_a_n_right[idx] = s_a_n_right[idx] - 1;
+	  }else if(rawdb(i,1)==e_truths[eid]){ // plus
+	    s_a_n_right[idx] = s_a_n_right[idx] + 1;
+	  }
+	}
+      }
+      
     }
     
     // sample output
     it=it+1;
     if((it > burnin) & (it % sample_step == 0)){
-      for(int l = 0; l < nfacts; ++l){
-	facts_out[l] = facts_out[l] + facts[l]/(double)sample_size;
-      }
-      for(int l = 0; l < ctsc.nrow(); ++l){
-	ctsc_out[l] = ctsc_out[l] + (double)ctsc(l,3)/(double)sample_size;
+      // for(int l = 0; l < nentities; ++l){
+      // 	e_truths_out[l]
+      // 	entities_out[l] = entities_out[l] + entities[l]/(double)sample_size;
+      // }
+      for(int s = 0; s < nsourceattributes; ++s){
+	s_a_n_right_out[s] = s_a_n_right_out[s] + (double)s_a_n_right[s] / (double)sample_size;
       }
     }
-    
+
     printProgress((double)it/maxit);
     
   }
   
-  
-  for(int s = 0; s < nsources; ++s){
-    // recall
-    idx=expand_source_claim+s*(int)2+1;
-    recall[s] = ((double)ctsc_out[idx]+alpha1(s,0))/
-      ((double)ctsc_out[idx]+(double)ctsc_out[idx-1]+alpha1(s,0)+alpha1(s,1));
-    
+  // esitmate source quality
+  for(int s = 0; s < nsourceattributes; ++s){
     // precision
-    precision[s] = ((double)ctsc_out[idx]+alpha1(s,1))/
-      ((double)ctsc_out[idx] + (double)ctsc_out[s*(int)2+(int)1] + alpha0(s,0) + alpha1(s,1));
-    
-    // specificity
-    idx=s*(int)2;
-    specificity[s] = ((double)ctsc_out[idx]+alpha0(s,0))/
-      ((double)ctsc_out[idx]+(double)ctsc_out[idx+1]+alpha0(s,0)+ alpha0(s,1));
+    precision[s] = ((double)s_a_n_right_out[s]+alpha1(s,0))/
+      ((double)s_a_n_claims[s] + alpha1(s,0) + alpha1(s,1));
   }
   
-  return Rcpp::List::create(Rcpp::Named("facts_out") = facts_out,
-			    Rcpp::Named("ctsc_out") = ctsc_out,
+  return Rcpp::List::create(Rcpp::Named("e_truths") = e_truths,
+			    Rcpp::Named("s_a_n_right_out") = s_a_n_right_out,
 			    Rcpp::Named("sample_size") = sample_size,
-			    Rcpp::Named("recall") = recall,
-			    Rcpp::Named("precision") = precision,
-			    Rcpp::Named("specificity") = specificity);
+			    Rcpp::Named("precision") = precision);
 }
